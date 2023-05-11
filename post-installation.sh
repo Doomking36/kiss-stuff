cfb06a85ab5257f30408addae13bd2e6f0a39c8a
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-05-11 04:07:43 -0500

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -18,7 +18,7 @@ git clone https://github.com/LukeSmithxyz/dwm.git
 git clone https://github.com/LukeSmithxyz/st.git
 
 rm -rf /home/dk/dmenu/dmenu.c
-mv kiss-stuff/dmenu.c /home/dk/dmenu
+mv kiss-stuff/dmenu.c /home/dk/dmenu/
 #cd dmenu
 #echo 123 | doas make PREFIX=/usr DESTDIR="$1" clean install
 #cd ..
