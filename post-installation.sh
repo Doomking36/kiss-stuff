e3395ba1bc5dbb610d88af12e71c8934154c6e11
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-05-11 04:39:16 -0500

    Update post-installation.sh

--- post-installation.sh
+++ post-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yes | kiss b python llvm cmake xauth xinit xorg-server xf86-input-libinput xf86-video-intel xf86-video-vesa xf86-video-fbdev xcompmgr feh neofetch pfetch htop
+yes | kiss b python llvm cmake xauth xinit xorg-server xf86-input-libinput xf86-video-intel xf86-video-vesa xf86-video-fbdev liberation-fonts xcompmgr feh neofetch pfetch htop
 
 #echo 123 | doas addgroup dk video
 #echo 123 | doas addgroup dk audio
